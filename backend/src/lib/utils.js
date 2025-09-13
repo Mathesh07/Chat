aa2653992943d888a19b3f84a7783818cd069ac0
Mathesh07 <mathesh4678@gmail.com>
2025-09-13 18:37:41 +0530

    Another cors change

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,9 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 1 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    httpOnly: true,
+    secure:true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "None", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development",
   });
 
