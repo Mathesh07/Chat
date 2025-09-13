506e4c684c568db3fe508c9c56322966acfbd97e
Mathesh07 <mathesh4678@gmail.com>
2025-09-13 15:15:16 +0530

    CORS changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,16 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "25mb" })); // or higher
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://chat-fawn-seven-40.vercel.app"   
+  ],
+  credentials: true
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
